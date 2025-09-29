c56273e7fc9352c1c46625d2c1a4b590ecf16197
Deepakmandle5 <deepakmandle555@gmail.com>
2025-09-29 23:41:32 +0530

    Create python2.py

---
+++ python2.py
@@ -0,0 +1,6 @@
+#comments and ecapsequence and print statment
+'''I about this for comment
+it is used for other than hash'''
+"""and also learn about this type of comment in codeing """
+print("I am newbie in programming\nand today I learn about escape sequence print statement and comments")
+print ("I am persuing \"b.tech\"")
