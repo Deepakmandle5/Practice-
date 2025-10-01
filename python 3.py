a2ab20bf80edf31b1b540b24713d65f20c6c8502
Deepakmandle5 <deepakmandle555@gmail.com>
2025-10-02 00:08:37 +0530

    Create python 3.py

---
+++ python 3.py
@@ -0,0 +1,50 @@
+"""today I  learn about variable and  data type.
+   In data type I learn about different type of data like
+    int, float and and complexes in numerical data 
+    in text type data it's string(str)
+    and in boolean data there is only{true} and {false} 
+    about sequence data like list and tuple 
+    where list is mutable but tuple is inmutabl means tuple deos not change."""
+    
+    
+a=12
+b=24
+c=27
+ #this all this is is variable and the data in it is numeric type data 
+a1="biginners"
+b1="coder"
+c1="learning python"
+    
+#now print all variables of each type 
+print(a)
+print(b) 
+print(c)
+
+ #do some addition
+print(a+b)
+print(c+b)
+print(a+c)
+print(a+b+c)
+
+print ("the type of a is", type (a))
+print ("the type of b is ",type(b))
+print("the type of c is",type(c))
+
+##now we wrote about text data type
+print(a1)
+print(b1) 
+print(c1)
+
+print(a1+b1)
+print(a1+b1+c1)
+
+
+print ("the type of a1 is", type(a1))
+print ("the type of b1 is", type(b1))
+print ("the type of c1 is", type(c1))
+
+
+# example of list 
+list1 =[1,2,3.1,[-1,-4],["skill","learner"]]
+print (list1)
+
