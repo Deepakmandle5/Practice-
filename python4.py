794194428bb08dda281decef99345d72d48f3a2b
Deepakmandle5 <deepakmandle555@gmail.com>
2025-10-04 17:21:17 +0530

    Create python4.py

---
+++ python4.py
@@ -0,0 +1,13 @@
+#toady I learn about the opration like 
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+"""this some special oprations (//) is removing decimal from it.
+and (***) is for multiple the digit for more then 2 times
+for example, 5*5*5*5*5 is written as 5**5
+and the last one is (5%5) give us the remainder of the division of 2 numbers
+for example 5%5 is 0 and 2%5 is 2"""
+print(5//5)
+print(5%5)
+print(5**5)
