773833bc7a4fe559828d93bff40bebeb2dc490ea
Deepakmandle5 <deepakmandle555@gmail.com>
2025-10-06 19:20:26 +0530

    Create python5.py

---
+++ python5.py
@@ -0,0 +1,7 @@
+#today i learned how to make basic calculator 
+a=5
+b=15
+print("the value of",a,"+",b,"is:",a+b)
+print("the value of",a,"-",b,"is:",a-b)
+print("the value of",a,"*",b,"is:",a*b)
+print("the value of",a,"/",b,"is:",a/b)
