2267f159c5582aad938c076adf9a682e3a5494e6
Deepakmandle5 <deepakmandle555@gmail.com>
2026-01-15 21:55:03 +0530

    pythonagain.7

---
+++ pythonagain.6.py
@@ -0,0 +1,36 @@
+print("hello world")
+a=2
+b=28
+c=55
+print(a+b+c)
+print(a*b*c)
+print(a-b-c)
+print(a/b/c)
+
+
+
+#typecasting
+# explicit typecasting 
+a = "1"
+b = "2"
+c = "3"
+print(int(a) + int(b) + int(c))
+
+#impicit typecasting
+d = 10
+e = 20.5
+print(d + e)  # d is implicitly converted to float 
+print(type(d))
+print(type(e))  
+print(type(d + e))
+
+
+# user input function
+a=input("Enter your name:")
+print("My name is ",a)
+
+x= input ("Enter First number ")
+y=input("Enter second number")
+print(x+y)
+
+print(int(x)+int(y))
