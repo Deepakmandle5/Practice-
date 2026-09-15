3cceb9acb0fdf10301fcb78618cc79a20a97afbf
Deepakmandle5 <deepakmandle555@gmail.com>
2026-09-15 22:32:32 +0530

    Update mathquiz mini project code

--- Python/PythonMiniproject18.py
+++ Python/PythonMiniproject18.py
@@ -1,73 +1,3 @@
-#Mini project
-
-#MATH QUIZ
-print("Q1:(100-50)*2")
-print("A =100","B =50","C =25","D =75")
-Answer = (input("Enter the answer:"))
-point = 0
-
-if Answer == "A":
-    print("Answer is correct.")
-    point = point +10
-else:
-    print("Answer is wrong.")
-
-if Answer == "A":
-    print("You get 10 points.")
-
-print("Q2:(2*2*4*4)")
-print("A =54","B =64","C =94","D =84")
-Answer = (input("Enter the answer:"))
-
-if Answer == "B":
-    print("Answer is correct.")
-    point = point +10
-else:
-    print("Answer is wrong.")
-
-if Answer == "B":
-    print("You get 10 points.")
-
-print("Q3:(38/2+20/4)") 
-print("A =55","B =64","C =24","D =37")
-Answer = (input("Enter the answer:"))   
-
-if Answer == "C":
-    print("Answer is correct.")
-    point = point +10
-else:
-    print("Answer is wrong.")
-    
-if Answer == "C":
-    print("You get 10 points.")
-
-print("Q4:(34*4)+(54/6)")
-print("A =105", "B =410","C =565","D =460")
-Answer = (input ("Enter the answer:"))
-
-if Answer == "D":
-    print("Answer is correct.")
-    point = point +10
-else:
-    print("Answer is wrong.")
-    
-if Answer == "D":
-    print("You get 10 points.")
-
-print("Q5:(37+9)-(64/8)")
-print("A =100","B = 38","C = 56","D =94")
-Answer = (input("Enter the answer:"))
-
-if Answer == "B":
-    print("Answer is correct.")
-    point = point+10
-else:
-    print("Answer is wrong.")
-
-print("Your total score is",point,"points")    
-if Answer == "B":
-    print("You get 10 points.")
-
 
 # Updated mini project
 # Maths Quiz
@@ -106,5 +36,3 @@ for q in Questions:
     i = i + 1
 
 print("Your total score is",point,"points")          
-
-
