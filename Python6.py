73f8bf050d06b2ae2ceb7dc8cb2a41f4476866c7
Deepakmandle5 <deepakmandle555@gmail.com>
2026-09-15 18:12:56 +0530

    Clean up whitespace and update print statements

--- Python6.py
+++ Python6.py
@@ -7,8 +7,6 @@ print(a*b*c)
 print(a-b-c)
 print(a/b/c)
 
-
-
 #typecasting
 # explicit typecasting 
 a = "1"
@@ -23,7 +21,6 @@ print(d + e)  # d is implicitly converted to float
 print(type(d))
 print(type(e))  
 print(type(d + e))
-
 
 # user input function
 a=input("Enter your name:")
